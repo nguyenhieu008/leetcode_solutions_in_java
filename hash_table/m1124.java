0b4bb3feeab222e1323def47bf9719507271b162
nguyenhieu008 <nguyenhieu008@gmail.com>
2025-02-20 23:15:17 +0700

    Update m1124.java

--- hash_table/m1124.java
+++ hash_table/m1124.java
@@ -1,5 +1,10 @@
 // https://leetcode.com/problems/longest-well-performing-interval/
 
+// We rate each day as +1, -1 score, then we calculate score of the whole subarray from beginning to i position.
+// If score > 0 => the whole subarray is WPI and we update the max WPI to (i + 1).
+// If not, then we store the first occurence of that score into a hash table. Because we go first with 0, so the first occurent of -1 will always smaller than -2, and so on.
+// Then for each pos i, with score < 0, we calculate the maximum possible longest WPI to i, by finding the first occurent of (score - 1) in the hash table. (score - 2) and others still yield a WPI, but not longest possible as above idea.
+
 class Solution {
     public int longestWPI(int[] hours) {
         int n = hours.length;
