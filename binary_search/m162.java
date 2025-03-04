fa03e04cc6bbf4bf7a40dd07638a72996ba37e15
nguyenhieu008 <nguyenhieu008@gmail.com>
2025-03-05 00:05:48 +0700

    Update m162.java

--- binary_search/m162.java
+++ binary_search/m162.java
@@ -1,11 +1,12 @@
 // https://leetcode.com/problems/find-peak-element/description/
 
 // Solution: Notice that the index 0, and n-1, in considered > than adjacent => we will always have peaks, even if the inner items are decreasing, two ends of array are peaks.
-// Now, let take an example, x denotes that items are less than the adjacent:
+// Now, let take an example, x denotes that items are less than the adjacent, so we have an array that both ends are greater than the adjacent items:
 // [x] 1 2 3 4 5 6 7 [x], mid = 4 => x x x x 5 6 7 x 
 // Because for peak, we only care about the adjacent item, so we remove the whole left of the mid (including mid). 
-// Because we can guarantee: "adjacent items of current array (x 5 6 7 x) will less than the current 2-ends of the array"
-// So, downgrade until we have 1-sized array, it stills guarantee the feature, both 2-adjacent items are less than the current array (include only 1 item).
+// We still guarantee: "an array that both ends are greater than the adjacent items (x 5 6 7 x)"
+// So, downgrade until we have 1-sized array, it stills guarantee the feature, both 2-adjacent items are less than the current array including only 1 item => it's the peak
+
 // Time complexity: O(logn);
 // Space complexity: O(1)
 
