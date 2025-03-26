1740c9cf197ce024fc9f6427f0912e9c4883a1d0
nguyenhieu008 <nguyenhieu008@gmail.com>
2025-03-26 23:33:42 +0700

    Create todo_h995.java

---
+++ array_string/todo_h995.java
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/minimum-number-of-k-consecutive-bit-flips/description/
+
+// Try to solve this again
+
+class Solution {
+    public int minKBitFlips(int[] A, int K) {
+        int n = A.length, flipped = 0, res = 0;
+        int[] isFlipped = new int[n];
+        for (int i = 0; i < A.length; ++i) {
+            if (i >= K)
+                flipped ^= isFlipped[i - K];
+            if (flipped == A[i]) {
+                if (i + K > A.length)
+                    return -1;
+                isFlipped[i] = 1;
+                flipped ^= 1;
+                res++;
+            }
+        }
+        return res;
+    }
+}
