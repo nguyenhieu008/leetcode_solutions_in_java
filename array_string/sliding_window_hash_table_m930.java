c4cedd02fdf8016a08c745ffb83b7bba52441c23
nguyenhieu008 <nguyenhieu008@gmail.com>
2025-03-31 01:09:33 +0700

    Update sliding_window_hash_table_m930.java

--- array_string/sliding_window_hash_table_m930.java
+++ array_string/sliding_window_hash_table_m930.java
@@ -78,27 +78,16 @@ class Solution {
 
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        int totalCount = 0;
-        int currentSum = 0;
-        // {prefix: number of occurrence}
-        Map<Integer, Integer> freq = new HashMap<>(); // To store the frequency of prefix sums
+        HashMap<Integer, Integer> map = new HashMap<>(Map.of(0, 1));
 
-        for (int num : nums) {
-            currentSum += num;
-            // THIS ONE IS KEY, SO WE do not need to freq.put(0, 1); then do not need to handle edge case for 0
-            if (currentSum == goal){
-                totalCount++;
-            }
-
-            // Check if there is any prefix sum that can be subtracted from the current sum to get the desired goal
-            if (freq.containsKey(currentSum - goal)){
-                totalCount += freq.get(currentSum - goal);
-            }
-
-            freq.put(currentSum, freq.getOrDefault(currentSum, 0) + 1);
+        int res = 0;
+        int sum = 0;
+        for (int v : nums) {
+            sum += v;
+            res += map.getOrDefault(sum - goal, 0);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
-
-        return totalCount;
+        return res;
     }
 }
 
