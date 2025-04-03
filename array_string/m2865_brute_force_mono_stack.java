d5a14cdc6099326c600d787526d6c25f9e12d27c
nguyenhieu008 <nguyenhieu008@gmail.com>
2025-04-03 11:15:32 +0700

    Update m2865_brute_force_mono_stack.java

--- array_string/m2865_brute_force_mono_stack.java
+++ array_string/m2865_brute_force_mono_stack.java
@@ -46,52 +46,40 @@ class Solution {
 class Solution {
     public long maximumSumOfHeights(int[] heights) {
         int n = heights.length;
-        long ans = 0;
-        Deque<long[]> stack = new LinkedList<>();
 
         long[] left = new long[n];
-        long sum = heights[0];
-        stack.addLast(new long[]{heights[0], 1});
-        left[0] = sum;
-        for (int i = 1; i < n; i++) {
-            int cnt = 1;
-            while (!stack.isEmpty() && heights[i] <= stack.peekLast()[0]) {
-                long height = stack.peekLast()[0];
-                long freq = stack.peekLast()[1];
-                cnt += freq;
-                sum -= height * freq;
-                stack.removeLast();
-            }
-
-            stack.addLast(new long[]{heights[i], cnt});
-            sum += stack.peekLast()[0] * cnt;
-            left[i] = sum;
-        }
-
-        stack = new LinkedList<>();
-        long[] right = new long[n];
-        sum = heights[n - 1];
-        stack.addLast(new long[]{heights[n - 1], 1});
-        right[n - 1] = sum;
-        for (int i = n - 2; i >= 0; i--) {
-            int cnt = 1;
-            while (!stack.isEmpty() && heights[i] <= stack.peekLast()[0]) {
-                long height = stack.peekLast()[0];
-                long freq = stack.peekLast()[1];
-                cnt += freq;
-                sum -= height * freq;
-                stack.removeLast();
-            }
-
-            stack.addLast(new long[]{heights[i], cnt});
-            sum += stack.peekLast()[0] * cnt;
-            right[i] = sum;
-        }
-
+        Stack<Integer> stack = new Stack<>(); // increasing, store the index of previous local peaks
+        stack.push(-1);
+        long curSum = 0;
         for (int i = 0; i < n; i++) {
-            ans = Math.max(ans, left[i] + right[i] - heights[i]);
+            while (stack.size() > 1 && heights[stack.peek()] >= heights[i]) {
+                int truncatedIdx = stack.pop();
+                curSum -= (long)(truncatedIdx - stack.peek()) * heights[truncatedIdx];
+            }
+            curSum += (long)(i - stack.peek()) * heights[i];
+            stack.push(i);
+            left[i] = curSum;
         }
 
-        return ans;
+        long[] right = new long[n];
+        stack.clear();
+        stack.push(n);
+        curSum = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            while (stack.size() > 1 && heights[stack.peek()] >= heights[i]) {
+                int truncatedIdx = stack.pop();
+                curSum -= (long)(stack.peek() - truncatedIdx) * heights[truncatedIdx];
+            }
+            curSum += (long)(stack.peek() - i) * heights[i];
+            stack.push(i);
+            right[i] = curSum;
+        }
+
+        long res = 0;
+        for (int i = 0; i < n; i++) {
+            res = Math.max(res, left[i] + right[i] - heights[i]);
+        }
+
+        return res;
     }
 }
