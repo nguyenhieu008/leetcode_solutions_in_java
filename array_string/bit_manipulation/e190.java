9a067a7570401ba798965a543ec575af9c49c9f3
nguyenhieu008 <nguyenhieu008@gmail.com>
2025-04-12 21:41:56 +0700

    Create e190.java

---
+++ array_string/bit_manipulation/e190.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/reverse-bits/description/
+
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            int bit = n & 1;
+            n >>= 1;
+            res <<= 1;
+            res |= bit;
+        }
+        return res;
+    }
+}
+
+// Reference: https://leetcode.com/problems/reverse-bits/solutions/1232842/java-c-0ms-o-1-time-complexity-in-place-detailed-explanation/
+// Divide and conquer
+
+public class Solution {
+    
+    public int reverseBits(int num) {
+        
+        num = ((num & 0xffff0000) >>> 16) | ((num & 0x0000ffff) << 16);
+        num = ((num & 0xff00ff00) >>> 8) | ((num & 0x00ff00ff) << 8);
+        num = ((num & 0xf0f0f0f0) >>> 4) | ((num & 0x0f0f0f0f) << 4);
+        num = ((num & 0xcccccccc) >>> 2) | ((num & 0x33333333) << 2);
+        num = ((num & 0xaaaaaaaa) >>> 1) | ((num & 0x55555555) << 1);
+        
+        return num;
+        
+    }
+}
