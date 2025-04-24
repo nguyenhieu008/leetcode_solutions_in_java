8612ab95912676b244da88566e4eefb2fef21016
nguyenhieu008 <nguyenhieu008@gmail.com>
2025-04-25 01:25:55 +0700

    Create bfs_level_order_m102_BinaryTreeLevelOrderTraversal.java

---
+++ tree/bfs_level_order_m102_BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) {
+            return new LinkedList<>();
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        List<List<Integer>> res = new LinkedList<>();
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new LinkedList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            res.add(level);
+        }
+        return res;
+    }
+}
