ca2cae3c0336f78e1e3c6b388fbd7d5ade93be3f
nguyenhieu008 <nguyenhieu008@gmail.com>
2025-05-16 16:44:34 +0700

    Create string_e2085_CountCommonWordsWithOneOccurrence.java

---
+++ hash_table/string_e2085_CountCommonWordsWithOneOccurrence.java
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/count-common-words-with-one-occurrence/description/
+
+// There are other solutions with 1 map only, but it's harder to read.
+class Solution {
+    public int countWords(String[] words1, String[] words2) {
+        Map<String, Integer> freq1 = new HashMap<>();
+        Map<String, Integer> freq2 = new HashMap<>();
+
+        for (String w : words1) {
+            freq1.put(w, freq1.getOrDefault(w, 0) + 1);
+        }
+        for (String w : words2) {
+            freq2.put(w, freq2.getOrDefault(w, 0) + 1);
+        }
+
+        int res = 0;
+        for (String w : freq1.keySet()) {
+            if (freq1.get(w) == 1 && freq2.getOrDefault(w, 0) == 1) {
+                res++;
+            }
+        }
+        return res;
+    }
+}
