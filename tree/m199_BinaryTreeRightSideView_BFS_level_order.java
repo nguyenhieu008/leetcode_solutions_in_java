5cec810a93cbffa03a296a30e22778494476a947
nguyenhieu008 <nguyenhieu008@gmail.com>
2025-05-28 17:42:20 +0700

    Update m199_BinaryTreeRightSideView_BFS_level_order.java

--- tree/m199_BinaryTreeRightSideView_BFS_level_order.java
+++ tree/m199_BinaryTreeRightSideView_BFS_level_order.java
@@ -34,6 +34,43 @@ class Solution {
     }
 }
 
+// Solution 3: DFS
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> res = new ArrayList<>();
+    public List<Integer> rightSideView(TreeNode root) {
+        dfs(root, 0);
+        return res;
+    }
+
+    private void dfs(TreeNode root, int level) {
+        if (root == null) {
+            return ;
+        }
+
+        if (res.size() <= level) {
+            res.add(root.val);
+        }
+
+        dfs(root.right, level + 1);
+        dfs(root.left, level + 1);
+    }
+}
+
 // Solution 1: Use null to separate levels
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
