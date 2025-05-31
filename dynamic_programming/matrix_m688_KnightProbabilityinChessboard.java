8b8e01671e03855e867798613100d5b4f5c7ac38
nguyenhieu008 <nguyenhieu008@gmail.com>
2025-06-01 02:23:15 +0700

    Update matrix_m688_KnightProbabilityinChessboard.java

--- dynamic_programming/matrix_m688_KnightProbabilityinChessboard.java
+++ dynamic_programming/matrix_m688_KnightProbabilityinChessboard.java
@@ -1,5 +1,77 @@
 // https://leetcode.com/problems/knight-probability-in-chessboard/description/
 
+// Solution 2a: Top-down
+class Solution {
+    private int[][] dirs = {{2, 1}, {2, -1}, {1, 2}, {1, -2}, {-1, 2}, {-1, -2}, {-2, 1}, {-2, -1}};
+    public double knightProbability(int n, int k, int r, int c) {
+        /*
+            A state can be define as (moves, row, col)
+            dp[move][row][col]: probability of the knight land at [row, col] when there are "move" moves left
+        */
+
+        double[][][] dp = new double[k+1][n][n];
+        return calculate(n, k, r, c, dp);
+    }
+
+    // k how many steps left
+    private double calculate(int n, int k, int row, int col, double[][][] dp) {
+        if (row < 0 || row >= n || col < 0 || col >= n) {
+            return 0L;
+        }
+        if (k == 0) {
+            return 1L;
+        }
+        if (dp[k][row][col] > 0) {
+            return dp[k][row][col];
+        }
+        double res = 0;
+        for (int[] d : dirs) {
+            res += 0.125 * calculate(n, k - 1, row + d[0], col + d[1], dp);
+        }
+        dp[k][row][col] = res;
+        return res;
+    }
+
+}
+
+// Solution 2: Top-down
+class Solution {
+    private int[][] dirs = {{2, 1}, {2, -1}, {1, 2}, {1, -2}, {-1, 2}, {-1, -2}, {-2, 1}, {-2, -1}};
+    public double knightProbability(int n, int k, int r, int c) {
+        /*
+            A state can be define as (moves, row, col)
+            Starting from [row, col], calling recusively with next step and next [nextRow, nextCol].
+            If it still in the board, continue to go. 
+            If step == k, then with that path, after k step, knight still in the board => return 1 as possibility
+            dp[move][row][col]: probability of the knight land at [row, col] when there are (k - move) left.
+        */
+
+        double[][][] dp = new double[k][n][n];
+        return calculate(n, 0, k, r, c, dp);
+    }
+
+    // k how many steps left
+    private double calculate(int n, int step, int k, int row, int col, double[][][] dp) {
+        if (row < 0 || row >= n || col < 0 || col >= n) {
+            return 0L;
+        }
+        if (step == k) {
+            return 1L;
+        }
+        if (dp[step][row][col] > 0) {
+            return dp[step][row][col];
+        }
+
+        double res = 0;
+        for (int[] d : dirs) {
+            res += 0.125 * calculate(n, step + 1, k, row + d[0], col + d[1], dp);
+        }
+        dp[step][row][col] = res;
+        return res;
+    }
+
+}
+
 // Solution 1a: Same as solution 1 but use 2 matrix then swap
 class Solution {
     public double knightProbability(int n, int k, int r, int c) {
