b4e95cbe127be0e00cbb2e17c76b2df7474787cd
nguyenhieu008 <nguyenhieu008@gmail.com>
2025-05-31 22:42:02 +0700

    Update level_order_m909_SnakesandLadders.java

--- graph/level_order_m909_SnakesandLadders.java
+++ graph/level_order_m909_SnakesandLadders.java
@@ -1,5 +1,66 @@
 // https://leetcode.com/problems/snakes-and-ladders/description/
-// Should use level order traversal, the distance to the last cell is the level
+
+// Solution 1a: Same as solution 1, but a bit shorter.
+// Mark visited for the destination cell of the snake/ladder right away => no need to if/else based on the value of a cell
+// => We also check visited a cell based on the destination, not just based on the nextSquare.
+// NOTICE: WE CAN USE 1 ARRAY FOR DISTANCE TO MARK VISITED AND SAVE THE DISTANCE AS WELL 
+// => NO NEED LEVEL ORDER TRANVERSAL (1 MORE NESTED WHILE);
+// Time complexity: O(n^2), n is the size of the edge of the board
+// Space complexity: O(n^2);
+
+class Solution {
+    public int snakesAndLadders(int[][] board) {
+        int n = board.length;
+        int maxSquare = n * n;
+
+        boolean[] visited = new boolean[maxSquare + 1];
+        visited[1] = true;
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(1);
+
+        int steps = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            while (size-- > 0) {
+                int curSquare = q.poll();
+                if (curSquare == maxSquare) {
+                    return steps;
+                }
+
+                for (int nextSquare = curSquare + 1; nextSquare <= Math.min(curSquare + 6, maxSquare); nextSquare++) {
+                    int nextValue = getSquareValue(board, nextSquare);
+                    int dest = nextValue != -1 ? nextValue : nextSquare;
+
+                    if (visited[dest]) {
+                        continue;
+                    }
+
+                    q.offer(dest);
+                    visited[dest] = true;
+                }
+            }
+            steps++;
+        }
+        return -1;
+    }
+
+    private int getSquareValue(int[][] board, int square) {
+        int n = board.length;
+        int baseRow = (square - 1) / n;
+        int baseCol = (square - 1) % n;
+        int row = n - baseRow - 1;
+        int col = 0;
+        if (baseRow % 2 == 0) {
+            col = baseCol;
+        } else {
+            col = n - baseCol - 1;
+        }
+
+        return board[row][col];
+    }
+}
+
+// Solution 1: Should use level order traversal, the distance to the last cell is the level
 
 class Solution {
     public int snakesAndLadders(int[][] board) {
