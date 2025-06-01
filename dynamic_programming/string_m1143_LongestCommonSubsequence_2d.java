034eb49b6bf45a6856dcf876a412a6ff8848b5e0
nguyenhieu008 <nguyenhieu008@gmail.com>
2025-06-01 22:21:46 +0700

    Update string_m1143_LongestCommonSubsequence_2d.java

--- dynamic_programming/string_m1143_LongestCommonSubsequence_2d.java
+++ dynamic_programming/string_m1143_LongestCommonSubsequence_2d.java
@@ -23,6 +23,40 @@ class Solution {
     }
 }
 
+// Solution 2: Top-down dp + memoize
+class Solution {
+    int[][] dp;
+    private int UNSET = -1;
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length(), n = text2.length();
+        dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(dp[i], UNSET);
+        }
+        
+        return lcs(text1, m - 1, text2, n - 1);
+    }
+
+    private int lcs(String s1, int i1, String s2, int i2) {
+        if (i1 < 0 || i2 < 0) {
+            return 0;
+        }
+
+        if (dp[i1][i2] != UNSET) {
+            return dp[i1][i2];
+        }
+
+        if (s1.charAt(i1) == s2.charAt(i2)) {
+            dp[i1][i2] = 1 + lcs(s1, i1 - 1, s2, i2 - 1);
+        } else {
+            int res = Math.max(lcs(s1, i1, s2, i2 - 1), lcs(s1, i1 - 1, s2, i2));
+            dp[i1][i2] = res;
+        }
+
+        return dp[i1][i2];
+    }
+}
+
 // Solution 1: DP
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
